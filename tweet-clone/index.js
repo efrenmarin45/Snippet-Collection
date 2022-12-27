e92b993807cb3f15c18141071f56d5d9f055da68
Efren_Marin <efren45marin@gmail.com>
2022-12-26 17:09:40 -0800

    feat: User Input Boops Implemented the ability for the user to submit their own boops and have it display on the feed. Added more comments to certain functions to better explain the purpose. Began work on a deletion button.

--- tweet-clone/index.js
+++ tweet-clone/index.js
@@ -1,9 +1,9 @@
 //* Data Source
 import { boopData } from "./data.js";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 //* Variables - DOM elements
 const boopInput = document.getElementById("boop-input");
-const boopBtn = document.getElementById("boop-btn");
 const boopFeed = document.getElementById("feed");
 
 //* Document Event Listener
@@ -11,6 +11,7 @@ document.addEventListener("click", (e) => {
 	let dataReplies = e.target.dataset.boopReplies;
 	let dataLikes = e.target.dataset.boopLikes;
 	let dataReboops = e.target.dataset.boopReboops;
+	let dataDelete = e.target.dataset.boopDelete;
 
 	if (dataReplies) {
 		handleReplyClick(dataReplies);
@@ -18,10 +19,14 @@ document.addEventListener("click", (e) => {
 		handleLikeClick(dataLikes);
 	} else if (dataReboops) {
 		handleReboopClick(dataReboops);
+	} else if (e.target.id === "boop-btn") {
+		handleInputBtnClick();
+	} else if (dataDelete) {
+		handleDeleteClick(dataDelete);
 	}
 });
 
-//* Checking to see if the data attribute matches the UUID of any object within the data source. Since there is only one UUID per user, we default to zero index and icrement by 1.
+//* Checking to see if the data attribute matches the UUID of any object within the data source. Since there is only one UUID per user, we default to zero index and increment by 1.
 const handleLikeClick = (boopID) => {
 	const targetBoopObj = boopData.filter((props) => {
 		return props.uuid === boopID;
@@ -57,15 +62,52 @@ const handleReboopClick = (boopID) => {
 	render();
 };
 
+//* Toggles the reply section on and off
 const handleReplyClick = (boopID) => {
-    document.getElementById(`replies-${boopID}`).classList.toggle('hidden');
+	document.getElementById(`replies-${boopID}`).classList.toggle("hidden");
 };
 
+//* Takes user boop and places it in the boopData array
+const handleInputBtnClick = () => {
+	if (boopInput.value){
+		boopData.unshift({
+			handle: "@PlayerOne",
+			profilePic: "img/default_avatar.jpg",
+			likes: 0,
+			reBoop: 0,
+			boopText: boopInput.value,
+			replies: [
+			],
+			isLiked: false,
+			isBooped: false,
+			uuid: uuidv4()
+		},);
+		render();
+		boopInput.value = '';
+	} 
+};
+
+//* Removes only user generated boops
+const handleDeleteClick = (boopID) => {
+	const targetBoopObj = boopData.filter((props) => {
+		return props.uuid === boopID;
+	})[0];
+	
+	if(targetBoopObj.handle === "@PlayerOne"){
+		console.log("boopData", boopData);
+		console.log("target", targetBoopObj.uuid);
+	}
+
+	render();
+};
+
+//* Displays the feed of boops
 const getFeedHTML = () => {
 	let feedHTML = ``;
 	boopData.forEach((boops) => {
 		let likeIcon = "";
 		let shareIcon = "";
+		let deleteIcon = "";
 
 		if (boops.isLiked) {
 			likeIcon = "liked";
@@ -73,8 +115,11 @@ const getFeedHTML = () => {
 		if (boops.isBooped) {
 			shareIcon = "reBoop";
 		}
+		if (boops.handle === "@PlayerOne"){
+			deleteIcon = "deleteBoop"
+		}
 
-		let repliesHTML = '';
+		let repliesHTML = "";
 
 		if (boops.replies.length > 0) {
 			boops.replies.forEach((replies) => {
@@ -99,16 +144,19 @@ const getFeedHTML = () => {
                             <p class="boop-text">${boops.boopText}</p>
                             <div class="boop-details">
                                 <span class="boop-detail">
-                                <i class="fa-regular fa-comment-dots" data-boop-replies="${boops.uuid}"></i> 
-                                    ${boops.replies.length}
+									<i class="fa-regular fa-comment-dots" data-boop-replies="${boops.uuid}"></i> 
+										${boops.replies.length}
                                 </span>
                                 <span class="boop-detail">
-                                <i class="fa-solid fa-heart ${likeIcon}" data-boop-likes="${boops.uuid}"></i>
-                                    ${boops.likes}
+									<i class="fa-solid fa-heart ${likeIcon}" data-boop-likes="${boops.uuid}"></i>
+										${boops.likes}
                                 </span>
                                 <span class="boop-detail">
-                                <i class="fa-solid fa-retweet ${shareIcon}" data-boop-reboops="${boops.uuid}"></i>
-                                    ${boops.reBoop}
+									<i class="fa-solid fa-retweet ${shareIcon}" data-boop-reboops="${boops.uuid}"></i>
+										${boops.reBoop}
+                                </span>
+								<span class="boop-detail">
+									<i class="fa-solid fa-trash ${deleteIcon}" data-boop-delete="${boops.uuid}"></i>
                                 </span>
                             </div>   
                         </div>            
@@ -121,6 +169,7 @@ const getFeedHTML = () => {
 	return feedHTML;
 };
 
+//* Renders the UI into the DOM
 const render = () => {
 	boopFeed.innerHTML = getFeedHTML();
 };
