ac894a3e7e1c7f5c6fdca536c78a825230592478
Efren_Marin <efren45marin@gmail.com>
2023-01-02 16:27:23 -0800

    Included API to draw cards and added them to the page.

--- card-wars/index.js
+++ card-wars/index.js
@@ -1,5 +1,8 @@
+//* Variables
 let deckID;
+const cardArea = document.getElementById('card-container');
 
+//* API Calls
 const handleNewDeckClick = () => {
 	fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 		.then((response) => response.json())
@@ -9,4 +12,18 @@ const handleNewDeckClick = () => {
 		});
 };
 
+const handleDrawCardClick = () => {
+	fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data.cards);
+		cardArea.innerHTML = `
+			<img src=${data.cards[0].image} />
+			<img src=${data.cards[1].image} />
+		`
+	})
+}
+
+//* Event Listeners
 document.getElementById('new-deck').addEventListener('click', handleNewDeckClick);
+document.getElementById('draw-card').addEventListener('click', handleDrawCardClick);
