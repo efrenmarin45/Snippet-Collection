4fc2be1552e6ff2cd0d9c1d904d1e924083b0fe3
Efren_Marin <efren45marin@gmail.com>
2023-01-02 19:04:51 -0800

    Included logic to determine winner per card draw

--- card-wars/index.js
+++ card-wars/index.js
@@ -1,6 +1,10 @@
 //* Variables
 let deckID;
+let houseCard;
+let playerCard;
 const cardArea = document.getElementById('card-container');
+const newDeck = document.getElementById('new-deck');
+const drawCard = document.getElementById('draw-card');
 
 //* API Calls
 const handleNewDeckClick = () => {
@@ -16,6 +20,8 @@ const handleDrawCardClick = () => {
 	fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
 	.then(response => response.json())
 	.then(data => {
+		houseCard = data.cards[0].value;
+		playerCard = data.cards[1].value;
 		console.log(data.cards);
 		cardArea.children[0].innerHTML = `
 			<img src=${data.cards[0].image} class="card" />
@@ -23,9 +29,27 @@ const handleDrawCardClick = () => {
 		cardArea.children[1].innerHTML = `
 		<img src=${data.cards[1].image} class="card" />
 		`
+		cardCompare(houseCard, playerCard)
 	})
 }
 
+//* Compare Drawn Cards
+const cardCompare = (cardOne, cardTwo) => {
+	const cardValueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+
+	const cardOneValueIndex = cardValueOptions.indexOf(cardOne);
+	const cardTwoValueIndex = cardValueOptions.indexOf(cardTwo);
+
+	if(cardOneValueIndex > cardTwoValueIndex){
+		return console.log("House card wins!")
+	} else if (cardOneValueIndex < cardTwoValueIndex){
+		return console.log("Player card wins!")
+	} else {
+		console.log("It's a tie!")
+	}
+}
+
 //* Event Listeners
-document.getElementById('new-deck').addEventListener('click', handleNewDeckClick);
-document.getElementById('draw-card').addEventListener('click', handleDrawCardClick);
+newDeck.addEventListener('click', handleNewDeckClick);
+drawCard.addEventListener('click', handleDrawCardClick);
